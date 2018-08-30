82b8d146abe09561da7c3422a20b9949fa8d6802
Juan Borda <juanignacioborda@gmail.com>
2018-08-30 15:39:03 -0300

    update build

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
- docker build -t juanitomint/php7 .
+time  docker build \
+        --build-arg VCS_REF=`git rev-parse --short HEAD` \
+        --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+        -t juanitomint/php7 . | tee build.log
