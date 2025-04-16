74e59863a9a9402ec6968d2636fe638eab95495e
Rodrigo Sebastian Gil <grodrigo.dev@gmail.com>
2025-04-16 14:11:34 -0300

    fix script build y push nombre de imagen

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 time docker build \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
         --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-        -t grodrigo/php:7.4-datospyme_api . | tee build.log
+        -t grodrigo/php:7.2-apache-buster_datospyme_api_v1 . | tee build.log
--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker push grodrigo/php:7.4-datospyme_api
+docker push grodrigo/php:7.2-apache-buster_datospyme_api_v1
 # notify microbadger
 #curl -X POST https://hooks.microbadger.com/images/juanitomint/php7/JElwBprtT-4SrAuHGvuCQjYUO3U=
